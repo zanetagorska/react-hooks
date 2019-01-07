dc3529e35b40ac97cc34fd259b9a927a051f7f82
zanetagorska <zaneta.gorska1@gmail.com>
2019-01-07 10:47:16 +0100

    Create resource list comonent with react hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+	const [ resource, setResource ] = useState('posts');
+	return (
+		<div className="App">
+			<button onClick={() => setResource('posts')}>Posts</button>
+			<button onClick={() => setResource('todos')}>Todos</button>
+			<ResourceList resource={resource} />
+		</div>
+	);
+};
 
 export default App;
